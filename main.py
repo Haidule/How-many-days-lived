d347ec6b9490920df0b8b3c4c87d4815d5d7ef60
Four$ <83818198+Haidule@users.noreply.github.com>
2022-03-11 11:50:34 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,13 +3,13 @@ from datetime import datetime
 
 def calculate():
     try:
-        # get the input date in DD/MM/YYYY format
+        # få inndatadatoen i formatet DD/MM/ÅÅÅÅ
         birth_date = inputdate_entry.get()
-        # split the date into day, month and year
+        # del opp datoen i dag, måned og år
         birth_day, birth_month, birth_year = birth_date.split('/')
-        # calculate the time difference from the input date to now
+        # beregn tidsforskjellen fra inndatadatoen til nå
         actual = datetime.now() - datetime(year=int(birth_year), month=int(birth_month), day=int(birth_day))
-        # show the number of days calculated
+        # vis antall beregnede dager
         output_entry.delete(0, tk.END)
         output_entry.insert(0, f'{actual.days} days')
     except Exception as ex:
@@ -24,18 +24,18 @@ root.resizable(False, False)
 inputdate = tk.Label(text="Type in your date (dd/mm/yyyy)")
 inputdate.place(x=50, y=30)
 
-# date entry (type your date)
+# datooppføring (skriv inn datoen din)
 inputdate_entry = tk.Entry(width=11)
 inputdate_entry.place(x=90, y=60)
 
 inputdate = tk.Label(text="Days! lived")
 inputdate.place(x=110, y=230)
 
-# calculates how many days you have lived , by pressing the calculate button
-enter_btn = tk.Button(text='Calculate', width=8, command=calculate) # call calculate()
+# beregner hvor mange dager du har levd, ved å trykke på beregn-knappen
+enter_btn = tk.Button(text='Calculate', width=8, command=calculate) # kall calculate()
 enter_btn.place(x=90, y=100)
 
-# here comes output and shows how many days you have lived
+# her kommer utdata og viser hvor mange dager du har levd
 output_entry = tk.Entry(width=11)
 output_entry.place(x=90, y=200)
 
